3efa9dfd6fb2f52f424a47d4ec9c27ac3c878804
Thomas <viltetomas2003@gmail.com>
2023-10-27 20:34:40 -0300

    add kafka compose

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/Tomas-vilte/FinanceStream/internal/kafka"
 	"github.com/Tomas-vilte/FinanceStream/internal/realtime"
 	"log"
+	"time"
 )
 
-func onDataReceived(data []byte) {
-	fmt.Printf("Data recibida: %s\n", string(data))
-}
-
 func main() {
+
+	// Configuración de Binance WebSocket
 	symbol := "btcusdt"
-	channels := []string{
-		symbol + "@bookTicker",
-		symbol + "@trade",
-		symbol + "@ticker",
-		symbol + "@aggTrade",
+	channel := "bookTicker"
+
+	binanceWS, err := realtime.NewBinanceWebSocket(symbol, channel)
+	if err != nil {
+		log.Fatal("Error al crear la conexión WebSocket:", err)
+		return
+	}
+	defer binanceWS.Close()
+
+	// Configuración de Kafka
+	brokerAddress := "localhost:9092"
+	kafkaTopic := "bookTicker"
+
+	kafkaProducer, err := kafka.NewKafkaProducer(brokerAddress, kafkaTopic)
+	if err != nil {
+		fmt.Printf("Error al crear el productor Kafka: %v\n", err)
+		return
+	}
+	defer kafkaProducer.Close()
+
+	// Función para manejar los datos recibidos desde Binance y publicar en Kafka
+	onDataReceived := func(data []byte) {
+		fmt.Printf("Datos recibidos de Binance: %s\n", string(data))
+
+		// Publica los datos en Kafka
+		if err := kafkaProducer.PublishData(data); err != nil {
+			fmt.Printf("Error al publicar en Kafka: %v\n", err)
+		}
 	}
 
-	err := realtime.SubscribeToBinanceStreams(channels, onDataReceived)
-	if err != nil {
-		log.Fatal("Error al suscribirse a los canales de Binance:", err)
-	}
+	// Suscripción a Binance WebSocket y procesamiento de datos
+	binanceWS.SubscribeToChannel(onDataReceived)
+
+	time.Sleep(10 * time.Minute)
 }
