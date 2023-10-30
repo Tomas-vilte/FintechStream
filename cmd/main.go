4667dad209d6310c2dd0065b949b4174657ff9aa
Thomas <viltetomas2003@gmail.com>
2023-10-30 15:46:39 -0300

    submit data to topic

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Tomas-vilte/FinanceStream/internal/config"
+	"github.com/Tomas-vilte/FinanceStream/internal/kafka"
 	"github.com/Tomas-vilte/FinanceStream/internal/realtime"
 	"log"
 	"time"
@@ -10,23 +10,31 @@ import (
 
 func main() {
 
-	onDataReceived := func(data []byte) {
-		fmt.Printf("Datos recibidos de Binance: %s\n", string(data))
+	kafkaConn, err := kafka.NewKafkaProducer("localhost:9092")
+	if err != nil {
+		log.Fatal("Error al crear la conexión a Kafka:", err)
+		return
 	}
+	defer kafkaConn.Close()
 
 	bookTickerConfig := config.ChannelConfig{
 		Symbol:     "btcusdt",
 		Channel:    "bookTicker",
-		KafkaTopic: "bookTicker",
+		KafkaTopic: "binanceBookTicker",
 	}
 
-	binanceWS, err := realtime.NewBinanceWebSocket([]config.ChannelConfig{bookTickerConfig})
+	bookTickerWS, err := realtime.NewBinanceWebSocket([]config.ChannelConfig{bookTickerConfig})
 	if err != nil {
 		log.Fatal("Error al crear la conexión WebSocket:", err)
 		return
 	}
-	defer binanceWS.Close()
-	go binanceWS.SubscribeToChannel(onDataReceived)
+	defer bookTickerWS.Close()
+	go bookTickerWS.SubscribeToChannel(func(data []byte) {
+		err := kafkaConn.PublishData("binanceBookTicker", data)
+		if err != nil {
+			log.Fatalf("Error al enviar datos a Kafka: %v\n", err)
+		}
+	})
 
 	tradeConfig := config.ChannelConfig{
 		Symbol:     "btcusdt",
@@ -40,7 +48,12 @@ func main() {
 		return
 	}
 	defer tradeWS.Close()
-	go tradeWS.SubscribeToChannel(onDataReceived)
+	go tradeWS.SubscribeToChannel(func(data []byte) {
+		err := kafkaConn.PublishData("binanceTrade", data)
+		if err != nil {
+			log.Fatalf("Error al enviar datos a Kafka: %v\n", err)
+		}
+	})
 
 	time.Sleep(1 * time.Minute)
 
