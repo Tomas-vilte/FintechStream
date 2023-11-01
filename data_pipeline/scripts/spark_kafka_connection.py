3a9334dc6e489a82f70931991a016ad3cc051f85
Thomas <viltetomas2003@gmail.com>
2023-11-01 13:53:06 -0300

    handler error

--- data_pipeline/scripts/spark_kafka_connection.py
+++ data_pipeline/scripts/spark_kafka_connection.py
@@ -1,19 +1,19 @@
+import logging
 from data_pipeline.config.topic_config import TOPICS_CONFIG
 from pyspark.sql import SparkSession
 
 
 def create_spark_session(app: str):
-    return (
-        SparkSession
-        .builder
-        .appName(name=app)
-        .getOrCreate())
+    try:
+        conn = SparkSession.builder \
+            .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0") \
+            .appName(name=app) \
+            .getOrCreate()
+        conn.sparkContext.setLogLevel("ERROR")
+        logging.info("Conexion creada con exito")
+    except Exception as e:
+        logging.error(f"Hubo un error al crear la session de spark: {e}")
 
 
-# spark = create_spark_session("test")
-#
-# read_stream = spark \
-#                 .readStream \
-#     .option(TOPICS_CONFIG[""])
 
-print(TOPICS_CONFIG["binanceBookTicker"]["topic"])
+
