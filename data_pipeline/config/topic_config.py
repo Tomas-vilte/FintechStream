1e47bc2922419c5e3c54cd266999de6060617abf
Thomas <viltetomas2003@gmail.com>
2023-11-07 15:38:35 -0300

    adding schema to configuration dict

--- data_pipeline/config/topic_config.py
+++ data_pipeline/config/topic_config.py
@@ -1,9 +1,14 @@
+from data_pipeline.schema.binance_book_ticker_schema import binance_json_schema
+from data_pipeline.schema.binance_ticker_schema import binance_json_ticker_schema
+
 TOPICS_CONFIG: dict = {
     "host": "broker:9092",
     "binanceBookTicker": {
         "topic": "binanceBookTicker",
+        "schema": binance_json_schema
     },
     "binanceTrade": {
         "topic": "binanceTrade",
+        "schema": binance_json_ticker_schema
     }
 }
