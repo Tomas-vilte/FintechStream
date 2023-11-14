14525d40e15ece2cb4562e19ac9c2dcf69efe1f7
Thomas <viltetomas2003@gmail.com>
2023-11-14 15:59:57 -0300

    add config

---
+++ config/superset_config.py
@@ -0,0 +1,5 @@
+# En la terminal podes ejecutar el comando openssl rand -base64 42, para que te genere una key random
+SECRET_KEY = 'AqCuMfEOASX8KCg6WJelsG83zZpOeO0Wc3YGiLE2lMLVS2l4koUUwZb0'
+
+
+SQLALCHEMY_DATABASE_URI="postgresql://superset:superset@postgres:5432/superset"
