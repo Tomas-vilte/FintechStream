1ec7ef15f92995b5a194486eff7fe782e8bea38b
Thomas <viltetomas2003@gmail.com>
2023-11-15 19:40:25 -0300

    add config

---
+++ docker/docker-entrypoint-initdb.d/create_user.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Espera a que el servicio de Superset esté listo
+until docker exec -it superset superset db upgrade
+do
+  echo "Esperando que Superset esté listo..."
+  sleep 2
+done
+
+# Comando para crear un usuario administrador utilizando variables de entorno
+docker exec -it superset superset fab create-admin \
+  --username "${ADMIN_USERNAME}" \
+  --firstname "${ADMIN_FIRSTNAME}" \
+  --lastname "${ADMIN_LASTNAME}" \
+  --email "${ADMIN_EMAIL}" \
+  --password "${ADMIN_PASSWORD}"
+
+# Migrar la base de datos local de Superset a la última versión
+docker exec -it superset superset db upgrade
+
+# Configurar roles
+docker exec -it superset superset init
